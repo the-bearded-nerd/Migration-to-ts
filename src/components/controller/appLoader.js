1d83a0b0fa6a0fb92ee3c3f1731cbb82de4c77b1
the-bearded-nerd <dmitrykarvonen@gmail.com>
2022-06-29 18:54:52 +0300

    fix: add apiKey to AppLoader

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'bebb84186690411999e90d40bd2fb672', // получите свой ключ https://newsapi.org/
         });
     }
 }
