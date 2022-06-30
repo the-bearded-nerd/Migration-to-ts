94f564d7c888bea1fdfbf22f411a27c88318cb19
the-bearded-nerd <dmitrykarvonen@gmail.com>
2022-06-30 23:07:10 +0300

    feat: add News APIs

---
+++ src/types/index.ts
@@ -0,0 +1,42 @@
+interface ISource {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+interface IArticleSource {
+    id: string | null;
+    name: string;
+}
+
+interface IArticle {
+    source: IArticleSource;
+    author: string | null;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string | null;
+    publishedAt: string;
+    content: string;
+}
+
+export interface ISourcesResponseObject {
+    status: string;
+    sources: ISource[];
+}
+
+export interface IArticlesResponseObject {
+    status: string;
+    titalResults: number;
+    articles: IArticle[];
+}
+
+export interface IError {
+    status: string;
+    code: string;
+    message: string;
+}
