9a84e4561f94b5b84d7d50f1082ce518a69e2adf
the-bearded-nerd <dmitrykarvonen@gmail.com>
2022-07-04 11:56:00 +0300

    fix: fix footer images

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index'),
@@ -34,6 +35,9 @@ const baseConfig = {
         }),
         new CleanWebpackPlugin(),
         new EslintPlugin({ extensions: 'ts' }),
+        new CopyPlugin({
+            patterns: [{ from: 'src/img', to: 'img' }],
+        }),
     ],
 };
 
